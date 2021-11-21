ec36d3b314565a55b2b60ec46d06758c4a84596f
Wallace Mendes <wallace.bmj@gmail.com>
2021-11-21 19:19:07 -0300

    Resolução Atividade

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,24 +4,37 @@ class ActivitiesController < ApplicationController
 
     def index
         # listar as atividades (variável activities)
-
+        @activities = Activity.all
         # criar uma atividade (variável activity)
+        @activity = Activity.new(name: "Atividade", date: Time.now.strftime("%d/%m/%Y"), description: "Descrição da atividade")
     end
     
     def create
         #Crie uma atividade aqui
-
+        @activity = Activity.new(activity_params)
         #Envie uma mensagem com flash[:msg] contendo o alerta
-
+        begin
+            @activity.save
+            @alert = "Atividade criada com sucesso!"
+        rescue
+            @alert = "Erro ao criar atividade!"
+        end
+        flash[:msg] = @alert
         write_alert_on_log
         redirect_to activities_index_url
     end
 
     def destroy
         #Destrua uma atividade por id
-
+        @activity = Activity.find(params[:id])
+        begin
+            @activity.destroy
+            @alert = "Atividade removida com sucesso!"
+        rescue
+            @alert = "Erro ao remover atividade!"
+        end
         #Envie uma mensagem com flash[:msg] contendo o alerta
-
+        flash[:msg] = @alert
         write_alert_on_log
         redirect_to activities_index_url
     end
@@ -30,6 +43,7 @@ class ActivitiesController < ApplicationController
 
     def activity_params
         #Defina os parâmetros de cada atividade em params
+        params.require(:activity).permit(:name, :date, :description)
     end
 
     def write_alert_on_log
